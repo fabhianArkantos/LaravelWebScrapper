cf04b78a8e39f624b9c31a5ab3b62b042812bb19
Fabián Villar Vergara <fabhian.villar@gmail.com>
2019-02-23 18:18:15 -0300

    Scraper Route and Controller created
    Scraper route and controller was created. For now, the controller only return a view with a generic phrase.

---
+++ app/Http/Controllers/ScraperController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScraperController extends Controller
+{
+    public function example(){
+        return view('scraper');
+    }
+}
